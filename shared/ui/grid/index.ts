f4136cbd257d8f87f2d2a09e22cf63f1fdc89692
hamaddo <hamaddo@yandex.ru>
2022-03-27 17:38:17 +0700

    feat: main page grid layout

--- shared/ui/grid/index.ts
+++ shared/ui/grid/index.ts
@@ -3,5 +3,6 @@ import styled from '@emotion/styled'
 export const Grid = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(auto-fill, max-content);
   gap: 32px 24px;
 `
