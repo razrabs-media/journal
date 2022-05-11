8d657b9ed16a38719a3c33a0e65c3a6de8a33880
Антон <101454913+raantoniv@users.noreply.github.com>
2022-05-12 01:46:02 +0300

    chore: update deployment

--- src/shared/api/apollo-client.ts
+++ src/shared/api/apollo-client.ts
@@ -2,6 +2,6 @@ import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
 
 export const client = new ApolloClient({
   ssrMode: true,
-  link: new HttpLink({ uri: process.env.NEXT_PUBLIC_GQL_ENDPOINT }),
+  link: new HttpLink({ uri: process.env.NEXT_PUBLIC_API_GATEWAY }),
   cache: new InMemoryCache().restore({}),
 })
