8c2224d7657d4f15db7e8c1645b6e5fbc073032a
Eugeny Almakov <darkzoner31@gmail.com>
2022-06-03 13:23:00 +0300

    feat: finished footer 2

--- src/shared/ui/footer/index.tsx
+++ src/shared/ui/footer/index.tsx
@@ -1,10 +1,35 @@
 import { FC } from 'react'
-import { Copyright, Logo, StyledFooter } from './styled'
+import {
+  Copyright,
+  Logo,
+  StyledFooter,
+  StyledFootMenu,
+  StyledMenuBlock,
+  StyledNav,
+  StyledShareNav,
+  StyledText,
+} from './styled'
 
 export const Footer: FC = () => (
   <StyledFooter>
-    <Copyright>In code we trust since 2021</Copyright>
-
+    <StyledFootMenu>
+      <Copyright>In code we trust since 2021</Copyright>
+      <StyledMenuBlock>
+        <StyledNav>
+          <a href='#'>о нас</a>
+          <a href='#'>условия</a>
+          <a href='#'>лицензия</a>
+          <a href='#'>связаться</a>
+        </StyledNav>
+        <StyledShareNav>
+          <StyledText>еще и тут:</StyledText>
+          <a href='#'>github</a>
+          <a href='#'>yt</a>
+          <a href='#'>tg</a>
+          <a href='#'>tw</a>
+        </StyledShareNav>
+      </StyledMenuBlock>
+    </StyledFootMenu>
     <Logo>Разрабы</Logo>
   </StyledFooter>
 )
--- src/shared/ui/footer/styled.ts
+++ src/shared/ui/footer/styled.ts
@@ -6,13 +6,6 @@ export const StyledFooter = styled.footer`
   padding: 20px 0 0 0;
   border-top: 1px solid ${({ theme }) => theme.colors.logo};
   overflow: hidden;
-`
-
-export const Copyright = styled.span`
-  text-align: left;
-
-  width: 140px;
-  height: 24px;
 
   font-style: normal;
   font-weight: 400;
@@ -20,6 +13,68 @@ export const Copyright = styled.span`
   line-height: 100%;
   letter-spacing: 1px;
   text-transform: uppercase;
+  font-feature-settings: 'salt' on;
+
+  color: ${({ theme }) => theme.colors.secondary};
+
+  @media screen and (max-width: 671px) {
+    height: 205px;
+  }
+`
+
+export const StyledFootMenu = styled.div`
+  display: flex;
+  justify-content: space-between;
+  @media screen and (max-width: 671px) {
+    flex-direction: column;
+    justify-content: flex-start;
+    gap: 20px;
+  }
+`
+
+export const StyledMenuBlock = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 62%;
+  @media screen and (max-width: 1319px) {
+    flex-direction: column;
+    align-items: flex-end;
+  }
+  @media screen and (max-width: 671px) {
+    width: 100%;
+    flex-direction: row;
+    justify-content: flex-start;
+  }
+`
+
+export const StyledNav = styled.nav`
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: space-between;
+  gap: 16px;
+  list-style-type: none;
+  a {
+    cursor: pointer;
+    color: ${({ theme }) => theme.colors.primary};
+  }
+  @media screen and (max-width: 671px) {
+    width: 135px;
+    flex-direction: column;
+  }
+`
+export const StyledShareNav = styled(StyledNav)``
+export const StyledText = styled.div`
+  color: ${({ theme }) => theme.colors.secondary};
+  @media screen and (max-width: 671px) {
+    display: none;
+  }
+`
+
+export const Copyright = styled.span`
+  text-align: left;
+  width: 140px;
+  height: 24px;
   font-feature-settings: 'salt' on;
   color: ${({ theme }) => theme.colors.secondary};
 `
@@ -43,6 +98,6 @@ export const Logo = styled.span`
   user-select: none;
 
   @media screen and (max-width: 671px) {
-    font-size: calc(var(--width) * 0.174);
+    display: none;
   }
 `
