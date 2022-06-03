122272981dc53a2b55b29dda0904ad882382dfc4
vekseel <alexeev.aleksandr@inbox.ru>
2022-06-03 09:28:36 +0300

    increased letter spacing in footer logo when width is greater than 1900px

--- src/shared/ui/footer/styled.ts
+++ src/shared/ui/footer/styled.ts
@@ -45,4 +45,8 @@ export const Logo = styled.span`
   @media screen and (max-width: 671px) {
     display: none;
   }
+
+  @media screen and (min-width: 1900px) {
+    letter-spacing: calc(var(--width) * 0.026);
+  }
 `
