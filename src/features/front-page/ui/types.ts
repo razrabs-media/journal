b305e2ecaf04c90ef41b2cf11f481a1ccb6f3bfb
Danil Tankov <danil.tankoff@yandex.ru>
2022-06-07 19:44:02 +0700

    fix: build

--- src/features/front-page/ui/types.ts
+++ src/features/front-page/ui/types.ts
@@ -1,8 +1,5 @@
-import { FC } from 'react'
-import { PostCardProps } from 'entities/posts'
 import { CurrentFrontPage } from '../model'
 
 export type Props = {
   frontPage: CurrentFrontPage['currentFrontPage']
-  PostCardComponent?: FC<PostCardProps>
 }
