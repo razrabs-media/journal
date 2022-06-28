75404cbae4126382227eeafcb01806fe2310a93d
eugeny-alkov <darkzoner31@gmail.com>
2022-06-29 00:48:09 +0300

    fix: comments fixed

--- src/shared/ui/footer/index.tsx
+++ src/shared/ui/footer/index.tsx
@@ -1,10 +1,75 @@
+import Typography from '@razrabs-ui/typography'
+import Link from 'next/link'
 import { FC } from 'react'
-import { Copyright, Logo, StyledFooter } from './styled'
+import {
+  Content,
+  Copyright,
+  Logo,
+  Menu,
+  MoreLabel,
+  Share,
+  StyledFooter,
+  StyledNav,
+} from './styled'
 
 export const Footer: FC = () => (
   <StyledFooter>
-    <Copyright>In code we trust since 2021</Copyright>
-
+    <Content>
+      <Copyright>
+        <Typography uppercase color='secondary' letterSpacing='1px' size='sm'>
+          In code we trust since 2021
+        </Typography>
+      </Copyright>
+      <Menu>
+        <StyledNav>
+          <Link href='#'>
+            <Typography uppercase letterSpacing='1px' size='sm'>
+              о нас
+            </Typography>
+          </Link>
+          <Link href='#'>
+            <Typography uppercase letterSpacing='1px' size='sm'>
+              условия
+            </Typography>
+          </Link>
+          <Link href='#'>
+            <Typography uppercase letterSpacing='1px' size='sm'>
+              лицензия
+            </Typography>
+          </Link>
+          <Link href='#'>
+            <Typography uppercase letterSpacing='1px' size='sm'>
+              связаться
+            </Typography>
+          </Link>
+        </StyledNav>
+        <Share>
+          <MoreLabel uppercase color='secondary' letterSpacing='1px' size='sm'>
+            еще и тут:
+          </MoreLabel>
+          <Link href='#'>
+            <Typography uppercase letterSpacing='1px' size='sm'>
+              github
+            </Typography>
+          </Link>
+          <Link href='#'>
+            <Typography uppercase letterSpacing='1px' size='sm'>
+              yt
+            </Typography>
+          </Link>
+          <Link href='#'>
+            <Typography uppercase letterSpacing='1px' size='sm'>
+              tg
+            </Typography>
+          </Link>
+          <Link href='#'>
+            <Typography uppercase letterSpacing='1px' size='sm'>
+              tw
+            </Typography>
+          </Link>
+        </Share>
+      </Menu>
+    </Content>
     <Logo>Разрабы</Logo>
   </StyledFooter>
 )
