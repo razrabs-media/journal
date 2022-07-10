ac13b4f9cfa322ffe2dfb2f3150f2f56edb19fa1
default-razrab <razrabs.heroku@gmail.com>
2022-07-11 00:58:35 +0300

    RAZ: fix modal hooks

--- src/shared/ui/modal/hooks.ts
+++ src/shared/ui/modal/hooks.ts
@@ -2,33 +2,15 @@ import { useEffect } from 'react'
 
 const showDialog = () => {
   const { body } = document
-  const scrollY = document.documentElement.style.getPropertyValue('--scroll-y')
-  body.style.position = 'fixed'
-  body.style.top = `-${scrollY}`
+  body.style.overflow = 'hidden'
 }
 const closeDialog = () => {
   const { body } = document
-  const scrollY = body.style.top
-  body.style.position = ''
-  body.style.top = ''
-  window.scrollTo(0, parseInt(scrollY || '0') * -1)
+  body.style.overflow = 'auto'
 }
-const lisener = () => {
-  document.documentElement.style.setProperty(
-    '--scroll-y',
-    `${window.scrollY}px`,
-  )
-}
-
 export const useDisableScroll = (hide: boolean) => {
   useEffect(() => {
-    window.addEventListener('scroll', lisener)
-
     if (hide) closeDialog()
     else showDialog()
-
-    return () => {
-      window.removeEventListener('scroll', lisener)
-    }
   }, [hide])
 }
