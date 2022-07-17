a502b8fb333738b75b5ef9f6a7a8c50bed657fc1
raantoniv <ra.anton.ivanov@yandex.ru>
2022-07-17 03:09:09 +0300

    fix: account badge font weight & height

--- src/shared/ui/button/types.ts
+++
@@ -1,7 +0,0 @@
-import { IconTypes } from '../icons/types'
-
-export type Props = {
-  children: string
-  icon: IconTypes
-  onClick: VoidFunction
-}
--- src/widgets/header/ui/preferences/styled.ts
+++ src/widgets/header/ui/preferences/styled.ts
@@ -1,8 +1,22 @@
 import styled from '@emotion/styled'
+import { Tag } from 'shared/ui'
 
 export const PreferencesBox = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
   gap: 16px;
+`
+
+export const AccountBadge = styled(Tag)`
+  font-weight: 500;
+`
+
+export const AuthButton = styled(Tag)`
+  background: none;
+  color: ${({ theme }) => theme.colors.primary};
+  &:active {
+    background: none;
+    color: ${({ theme }) => theme.colors.primary};
+  }
 `
