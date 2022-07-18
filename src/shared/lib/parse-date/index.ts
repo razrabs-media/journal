f76fd45d415749b7c42b801459df8e7c70a66055
raantoniv <ra.anton.ivanov@yandex.ru>
2022-07-19 02:18:55 +0300

    fix(commets): change format comment date

--- src/shared/lib/parse-date/index.ts
+++ src/shared/lib/parse-date/index.ts
@@ -2,27 +2,60 @@ import {
   format,
   formatDistanceStrict,
   isBefore,
+  isThisYear,
   parseISO,
   subDays,
 } from 'date-fns'
 import { ru } from 'date-fns/locale'
 
-export function parseDate(date?: string, distance = 7): string | undefined {
-  if (!date) {
-    return undefined
+type Options = {
+  distance?: number
+  format?: 'short'
+}
+
+export function parseDate(date: string, options?: Options): string | undefined {
+  const fromIso = parseISO(date)
+  const isShortFormat = options?.format === 'short'
+
+  const greaterThanWeek = isBefore(
+    fromIso,
+    subDays(new Date(), options?.distance ?? 7),
+  )
+
+  if (!isThisYear(fromIso)) {
+    const formattedDate = format(
+      fromIso,
+      `d ${isShortFormat ? 'MMM' : 'MMMM'} yyyy`,
+      {
+        locale: ru,
+      },
+    )
+
+    return isShortFormat ? formattedDate.replace('.', '') : formattedDate
   }
 
-  const fromIso = parseISO(date)
-
-  // Если больше, чем distance (7) дней назад - показываем полную дату
-  const isFullDate = isBefore(fromIso, subDays(new Date(), distance))
-
-  return isFullDate
-    ? format(fromIso, 'd MMMM, H:mm', {
+  if (greaterThanWeek) {
+    const formattedDate = format(
+      fromIso,
+      `d ${isShortFormat ? 'MMM' : 'MMMM'}`,
+      {
         locale: ru,
-      })
-    : formatDistanceStrict(fromIso, new Date(), {
-        locale: ru,
-        addSuffix: true,
-      })
+      },
+    )
+
+    return isShortFormat ? formattedDate.replace('.', '') : formattedDate
+  }
+
+  const formattedDistanceDate = formatDistanceStrict(fromIso, new Date(), {
+    locale: ru,
+    addSuffix: !isShortFormat,
+  })
+
+  return isShortFormat
+    ? formattedDistanceDate
+        .replace(/с([а-я]+)/gi, 'сек')
+        .replace(/м([а-я]+)/gi, 'м')
+        .replace(/ч([а-я]+)/gi, 'ч')
+        .replace(/д([а-я]+)/gi, 'д')
+    : formattedDistanceDate
 }
