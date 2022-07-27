24e0cf2d8aab2367722903b9050915ffa11bdf22
default-razrab <razrabs.heroku@gmail.com>
2022-07-27 22:34:46 +0300

    RAZ-246: Fixed short comments

--- src/widgets/comments/ui/comments-wrapper/index.ts
+++ src/widgets/comments/ui/comments-wrapper/index.ts
@@ -3,12 +3,13 @@ import { Props } from './types'
 
 export const CommentsWrapper = styled.div<Props>`
   display: grid;
-
   grid-template:
     'header' auto
     'content' 1fr
     'action' auto /
     1fr;
+
+  width: 100%;
 
   background: ${({ theme }) => theme.colors.backgroundSecondary};
 `
