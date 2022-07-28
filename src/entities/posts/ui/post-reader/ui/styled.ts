76e822af2373d645deb1ba5796560aa347158360
raantoniv <ra.anton.ivanov@yandex.ru>
2022-07-28 03:14:27 +0300

    fix(article): spacing after header

--- src/entities/posts/ui/post-reader/ui/styled.ts
+++ src/entities/posts/ui/post-reader/ui/styled.ts
@@ -4,6 +4,7 @@ import { MarkdownRenderer } from 'shared/ui'
 export const StyledReader = styled.div``
 
 export const ContentWrapper = styled.main<{ open: boolean }>`
+  margin-top: 76px;
   display: grid;
 
   grid-template-columns: minmax(0, 1fr) minmax(0, 2fr);
@@ -16,8 +17,8 @@ export const ContentWrapper = styled.main<{ open: boolean }>`
     grid-template-areas: '${({ open }) => (open ? 'content' : '. content')}';
   }
 
-  @media screen and (min-width: 1000px) {
-    margin-top: 50px;
+  @media screen and (min-width: 672px) {
+    margin-top: 74px;
   }
 
   @media screen and (max-width: 999px) {
@@ -29,4 +30,11 @@ export const ContentWrapper = styled.main<{ open: boolean }>`
 
 export const StyledRenderer = styled(MarkdownRenderer)`
   grid-area: content;
+  & :first-child {
+    margin-top: 0;
+  }
+
+  & :last-child {
+    margin-bottom: 0;
+  }
 `
