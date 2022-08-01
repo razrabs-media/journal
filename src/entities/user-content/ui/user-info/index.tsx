42977a4378da3335c7455f297f3cf751d340f966
Alexandr Sidorenko <auroragamesproject@gmail.com>
2022-08-02 05:28:20 +0700

    Fix pluralize naming and linting

--- src/entities/user-content/ui/user-info/index.tsx
+++ src/entities/user-content/ui/user-info/index.tsx
@@ -2,22 +2,40 @@ import Typography from '@razrabs-ui/typography'
 import { format, formatDistanceToNow } from 'date-fns'
 import { ru } from 'date-fns/locale'
 import { FC } from 'react'
+import { pluralize } from 'shared/lib'
 import { Flex } from 'shared/ui'
 import { Props } from './types'
-import { pluralize } from "shared/lib"
 
 export const UserInfo: FC<Props> = ({ date, postsCount, commentsCount }) => {
   const formattedDate = format(new Date(date), 'dd MMMM yyyy', {
     locale: ru,
   })
   const duration = formatDistanceToNow(new Date(date), { locale: ru })
-  const formattedPosts = pluralize({ zero: 'материал открыт', one: 'материала открыто', few: 'материалов открыто' }, postsCount)
-  const formattedComments = pluralize({ zero: 'коммент написан', one: 'коммента написано', few: 'комментов написано' }, commentsCount)
+  const formattedPosts = pluralize(
+    {
+      zero: 'материалов открыто',
+      one: 'материал открыт',
+      two: 'материала открыто',
+    },
+    postsCount,
+  )
+  const formattedComments = pluralize(
+    {
+      zero: 'комментов написано',
+      one: 'коммент написан',
+      two: 'коммента написано',
+    },
+    commentsCount,
+  )
 
   return (
     <Flex direction='column' gap={8}>
-      <Typography size='lg'>{postsCount} {formattedPosts}</Typography>
-      <Typography size='lg'>{commentsCount} {formattedComments}</Typography>
+      <Typography size='lg'>
+        {postsCount} {formattedPosts}
+      </Typography>
+      <Typography size='lg'>
+        {commentsCount} {formattedComments}
+      </Typography>
 
       <Flex gap={16}>
         <Typography size='lg'>С Разрабами с {formattedDate}</Typography>
--- src/shared/lib/pluralize/pluralize.ts
+++ src/shared/lib/pluralize/pluralize.ts
@@ -1,14 +1,14 @@
 /*eslint max-params: ["error", 3]*/
 /*eslint-env es6*/
 export const pluralize = (
-  { zero, one, few }: { zero: string, one: string, few: string },
+  { zero, one, two }: { zero: string; one: string; two: string },
   number = 0,
   cases = [2, 0, 1, 1, 1, 2],
 ): string => {
-  const titles = [zero, one, few]
+  const titles = [one, two, zero]
   return titles[
     number % 100 > 4 && number % 100 < 20
       ? 2
       : cases[number % 10 < 5 ? number % 10 : 5]
-    ]
+  ]
 }
