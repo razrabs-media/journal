3eb1d0b5f83511d3d35a2f849e48ae9fd38f0c77
Alexandr Sidorenko <auroragamesproject@gmail.com>
2022-08-02 04:45:52 +0700

    Added return type for pluralize

--- src/shared/lib/pluralize/pluralize.ts
+++ src/shared/lib/pluralize/pluralize.ts
@@ -4,7 +4,7 @@ export const pluralize = (
   { zero, one, few }: { zero: string, one: string, few: string },
   number = 0,
   cases = [2, 0, 1, 1, 1, 2],
-) => {
+): string => {
   const titles = [zero, one, few]
   return titles[
     number % 100 > 4 && number % 100 < 20
