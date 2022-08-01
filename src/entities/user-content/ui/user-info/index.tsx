ce9a07fee20051fbdad1962a891aaedc6b511500
Alexandr Sidorenko <auroragamesproject@gmail.com>
2022-08-02 04:39:48 +0700

    Reuse pluralize function

--- src/entities/user-content/ui/user-info/index.tsx
+++ src/entities/user-content/ui/user-info/index.tsx
@@ -4,31 +4,20 @@ import { ru } from 'date-fns/locale'
 import { FC } from 'react'
 import { Flex } from 'shared/ui'
 import { Props } from './types'
-
-const countFormatter = (titles: string[]) => {
-  return (number: number): string => {
-    const cases = [2, 0, 1, 1, 1, 2]
-
-    return titles[
-      number % 100 > 4 && number % 100 < 20
-        ? 2
-        : cases[number % 10 < 5 ? number % 10 : 5]
-      ]
-  }
-}
+import { pluralize } from "shared/lib"
 
 export const UserInfo: FC<Props> = ({ date, postsCount, commentsCount }) => {
   const formattedDate = format(new Date(date), 'dd MMMM yyyy', {
     locale: ru,
   })
   const duration = formatDistanceToNow(new Date(date), { locale: ru })
-  const postsFormatter = countFormatter(['материал открыт', 'материала открыто', 'материалов открыто'])
-  const commentsFormatter = countFormatter(['коммент написан', 'коммента написано', 'комментов написано'])
+  const formattedPosts = pluralize(['материал открыт', 'материала открыто', 'материалов открыто'], postsCount)
+  const formattedComments = pluralize(['коммент написан', 'коммента написано', 'комментов написано'], commentsCount)
 
   return (
     <Flex direction='column' gap={8}>
-      <Typography size='lg'>{postsCount} {postsFormatter(postsCount)}</Typography>
-      <Typography size='lg'>{commentsCount} {commentsFormatter(commentsCount)}</Typography>
+      <Typography size='lg'>{postsCount} {formattedPosts}</Typography>
+      <Typography size='lg'>{commentsCount} {formattedComments}</Typography>
 
       <Flex gap={16}>
         <Typography size='lg'>С Разрабами с {formattedDate}</Typography>
