9e58cd652fb719aa7f77129d3affcf54606ab028
Alexandr Sidorenko <8469236+batyshkaLenin@users.noreply.github.com>
2022-08-02 15:15:46 +0700

    Numerical inflection has been improved (#295)
    * Numerical inflection has been improved
    
    * Fix phrase formatter
    
    * Reuse pluralize function
    
    * Improve pluralize method
    
    * Added return type for pluralize
    
    * Fix pluralize naming and linting

--- src/entities/user-content/ui/user-info/index.tsx
+++ src/entities/user-content/ui/user-info/index.tsx
@@ -2,6 +2,7 @@ import Typography from '@razrabs-ui/typography'
 import { format, formatDistanceToNow } from 'date-fns'
 import { ru } from 'date-fns/locale'
 import { FC } from 'react'
+import { pluralize } from 'shared/lib'
 import { Flex } from 'shared/ui'
 import { Props } from './types'
 
@@ -10,11 +11,31 @@ export const UserInfo: FC<Props> = ({ date, postsCount, commentsCount }) => {
     locale: ru,
   })
   const duration = formatDistanceToNow(new Date(date), { locale: ru })
+  const formattedPosts = pluralize(
+    {
+      zero: 'материалов открыто',
+      one: 'материал открыт',
+      two: 'материала открыто',
+    },
+    postsCount,
+  )
+  const formattedComments = pluralize(
+    {
+      zero: 'комментов написано',
+      one: 'коммент написан',
+      two: 'коммента написано',
+    },
+    commentsCount,
+  )
 
   return (
     <Flex direction='column' gap={8}>
-      <Typography size='lg'>{postsCount} материалов открыто</Typography>
-      <Typography size='lg'>{commentsCount} комментов написано</Typography>
+      <Typography size='lg'>
+        {postsCount} {formattedPosts}
+      </Typography>
+      <Typography size='lg'>
+        {commentsCount} {formattedComments}
+      </Typography>
 
       <Flex gap={16}>
         <Typography size='lg'>С Разрабами с {formattedDate}</Typography>
--- src/shared/lib/pluralize/pluralize.ts
+++ src/shared/lib/pluralize/pluralize.ts
@@ -1,12 +1,14 @@
 /*eslint max-params: ["error", 3]*/
 /*eslint-env es6*/
 export const pluralize = (
-  txt: string[],
+  { zero, one, two }: { zero: string; one: string; two: string },
   number = 0,
   cases = [2, 0, 1, 1, 1, 2],
-) =>
-  txt[
+): string => {
+  const titles = [one, two, zero]
+  return titles[
     number % 100 > 4 && number % 100 < 20
       ? 2
       : cases[number % 10 < 5 ? number % 10 : 5]
   ]
+}
